118bcf584070adadec0f8caccebcd4244d2f2867
Robson <robccorreia@gmail.com>
2022-08-15 18:12:16 -0300

    feat: add routes and controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import router from "../routes";
+
+export const home = (req: Request, res: Response) => {
+  res.send('home no controller')
+}
+export const dogs = (req: Request, res: Response) => {
+
+}
+export const cats = (req: Request, res: Response) => {
+
+}
+export const fishes = (req: Request, res: Response) => {
+
+}
+
