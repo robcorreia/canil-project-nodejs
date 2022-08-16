ac955c096a3678d6b8c40b4273b83bc021e5c317
Robson <robccorreia@gmail.com>
2022-08-16 11:21:26 -0300

    feat: add controller

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,40 +1,53 @@
 import { Request, Response } from "express";
 import { createMenuObject } from '../helpers/createMenuObject'
+import { Pet } from "../models/pet";
 
 export const home = (req: Request, res: Response) => {
+  const list = Pet.getAll()
+
   res.render('pages/page', {
     menu: createMenuObject('all'),
     banner: {
       title: 'Todos os animais',
       background: 'allanimals.jpg'
-    }
+    },
+    list
   })
 }
 export const dogs = (req: Request, res: Response) => {
+  const listDogs = Pet.getFromType('dog');
+
   res.render('pages/page', {
     menu: createMenuObject('dog'),
     banner: {
       title: 'Cachorros',
       background: 'banner_dog.jpg'
-    }
+    },
+    list: listDogs
   })
 }
 export const cats = (req: Request, res: Response) => {
+  const listCats = Pet.getFromType('cat');
+
   res.render('pages/page', {
     menu: createMenuObject('cat'),
     banner: {
       title: 'Gatos',
       background: 'banner_cat.jpg'
-    }
+    },
+    list: listCats
   })
 }
 export const fishes = (req: Request, res: Response) => {
+  const listFishes = Pet.getFromType('fish');
+
   res.render('pages/page', {
     menu: createMenuObject('fish'),
     banner: {
       title: 'Peixes',
       background: 'banner_fish.jpg'
-    }
+    },
+    list: listFishes
   })
 }
 
