488682f1aab1346800d21489de6c3f4b86406889
Robson <robccorreia@gmail.com>
2022-08-16 14:29:19 -0300

    feat: add 404 page and search feature

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
+  const queryParams: string = req.query.q as string;
 
+  if (!queryParams) {
+    res.redirect('/')
+    return
+  }
+
+  const petList = Pet.getFromName(queryParams)
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list: petList,
+    queryParams
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,6 @@ server.use(express.static(path.join(__dirname, '../public')))
 server.use(mainRoutes);
 
 server.use((req, res) => {
-  res.send('Pagina não encontrada!')
+  res.render('pages/404')
 });
 server.listen(process.env.PORT)
